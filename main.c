403ef2d3158fafc53a2e6e9abc2b18d2e01fa87f
Josias Soares <61014395+josiassoares@users.noreply.github.com>
2020-09-24 16:31:44 -0300

    Main Adicionado

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<locale.h>
+#include"ListaEstoque/ListaEstoque.h"
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    Lista *ListaEstoque;
+    TipoProduto DadosProduto;
+
+    ListaEstoque = criaLista();
+
+    printf("O programa executou!!!");
+    
+    liberaLista(ListaEstoque);
+
+    return 0;
+}
